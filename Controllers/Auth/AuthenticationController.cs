6e522762342374ddcbf8c474144b1c5d45eeab2e
Sanji276 <bibeksharma225@gmail.com>
2024-07-08 21:34:26 +0545

    Add project files.

---
+++ Controllers/Auth/AuthenticationController.cs
@@ -0,0 +1,39 @@
+﻿using CrudApplication.Models.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudApplication.Controllers.Auth
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserLoginModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if(model.Username == "Admin" && model.Password == "12345")
+                    {
+                        return RedirectToAction("Index","Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("Error","Invalid Credentials");
+                    return View(model);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/Auth/AuthModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudApplication.Models.Auth
+{
+    public class AuthModel
+    {
+
+    }
+
+    public class UserLoginModel
+    {
+        [Required(ErrorMessage = "Username field is required.")]
+        [MaxLength(10)]
+        [MinLength(5)]
+        public string? Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
